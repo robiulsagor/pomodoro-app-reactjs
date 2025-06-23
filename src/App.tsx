c2b8cc5982353e1c351494d9393f4115e7bbd8b6
robiulsagor <robiul100.me@gmail.com>
2025-06-23 10:35:22 +0600

    Add background color to main container for improved aesthetics

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Home from "./pages/Home"
 
 const App = () => {
   return (
-    <main className="w-full h-screen flex items-center justify-center">
+    <main className="w-full h-screen bg-gray-950 flex items-center justify-center">
       <Home />
     </main>
   )
